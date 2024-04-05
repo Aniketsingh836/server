584ca8fc37939529ae0ca9777b7e1586b51d051a
Sarthak Teotia <sarthak.clevdoc@gmail.com>
2024-04-06 02:11:21 +0530

    final commit

--- src/models/user.js
+++ src/models/user.js
@@ -2,13 +2,20 @@
 
 const mongoose = require('mongoose');
 
-const userSchema = new mongoose.Schema({
-  username: {type :String , default : null},
-  key: {type :String , default : null},
-  expires_in: {type :Date , default : null},
-  generated_at:{type :Date , default : null}
-});
+// const userSchema = new mongoose.Schema({
+//   username: {type :String , default : null},
+//   key: {type :String , default : null},
+//   expires_in: {type :Date , default : null},
+//   generated_at:{type :Date , default : null}
+// });
 
+
+const userSchema = new mongoose.Schema({
+  username: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  sessionToken: { type: String, default: '' },
+  sessionExpires: { type: Date, default: Date.now },
+});
 
 
 module.exports = mongoose.model('User', userSchema);
